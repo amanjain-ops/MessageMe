cf7f44a2355ef9bc69b37716816f98d3a5bf5e10
amanjain-ops <jainaman06060@gmail.com>
2024-10-11 18:00:10 +0530

    login ui added

--- MessageMe/Views/Login/View/LoginView.swift
+++ MessageMe/Views/Login/View/LoginView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct LoginView: View {
+    @EnvironmentObject var viewModel: LoginViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Login")
+                .font(.title)
+                .fontWeight(.semibold)
+            
+            
+            TextField("Email", text: $viewModel.loginUser.email)
+                .autocapitalization(.none)
+                .frame(width: 350, height: 50)
+                .padding(.horizontal, 10)
+                .background(Color.gray.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            SecureField("Password", text: $viewModel.loginUser.password)
+                .autocapitalization(.none)
+                .frame(width: 350, height: 50)
+                .padding(.horizontal, 10)
+                .background(Color.gray.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            
+            Button {
+                viewModel.login()
+            } label: {
+                Text("Login")
+                    .foregroundStyle(.white)
+                    .frame(width: 350, height: 50)
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                
+            }
+            
+            Text(viewModel.loginResponsee?.accessToken ?? "No response")
+        }
     }
 }
 
 #Preview {
     LoginView()
+        .environmentObject(LoginViewModel())
 }
