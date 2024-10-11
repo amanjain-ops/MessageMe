2d7f85fd9e4e452a0d54f69179b4b6810c97af7f
amanjain-ops <jainaman06060@gmail.com>
2024-10-11 13:24:34 +0530

    Initial Commit

---
+++ MessageMe/MessageMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageMeApp.swift
+//  MessageMe
+//
+//  Created by Aman Jain on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MessageMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
