d0bead24a05f66dc1e88a7d50afb2a154610ad50
amanjain-ops <jainaman06060@gmail.com>
2024-10-11 17:31:03 +0530

    add login model

--- MessageMe/Views/Login/Model/Login.swift
+++ MessageMe/Views/Login/Model/Login.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Login: Codable {
+    var email: String = ""
+    var password: String = ""
+}
+
+struct LoginResponseq: Codable {
+    var accessToken: String?
+    var msg: String?
+    var user_id: String?
+    var error: String?
+}
