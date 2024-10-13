b55b24fb3cc7559e523ee272d6f3e8fa1c70532a
amanjain-ops <jainaman06060@gmail.com>
2024-10-14 02:46:03 +0530

    Added view model for ChatDetail

--- MessageMe/Views/Chats/Model/ChatDetailViewModel.swift
+++ MessageMe/Views/Chats/Model/ChatDetailViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+
+class ChatDetailViewModel: ObservableObject {
+    @Published var messages: [Messages] = []
+    @Published var isLoading = false
+    @Published var chatId = ChatId()
+    
+    func getMessages(with viewModel: LoginViewModel) {
+        Task {@MainActor in
+            isLoading = true
+            print(isLoading)
+            do {
+                print(chatId.chatId)
+                messages = try await NetworkManger.shared.getMessages(with: viewModel, chatId: chatId)
+                isLoading = false
+                
+            } catch {
+                throw MMError.invalidData
+            }
+        }
+    }
+}
