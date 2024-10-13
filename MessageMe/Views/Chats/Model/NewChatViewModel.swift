a79d55144bd1956f6862e37689914cd396b45d34
amanjain-ops <jainaman06060@gmail.com>
2024-10-14 02:48:32 +0530

    view model added for user list

--- MessageMe/Views/Chats/Model/NewChatViewModel.swift
+++ MessageMe/Views/Chats/Model/NewChatViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class NewChatViewModel: ObservableObject {
+    @Published var userList: [Profile] = []
+    
+    
+    func getUsers(with viewLogin: LoginViewModel) {
+        Task{@MainActor in
+            do {
+                let userListUnfilter = try await NetworkManger.shared.getUsers(with: viewLogin)
+                userList = userListUnfilter.filter { $0.id != viewLogin.loginResponsee?.userId}
+            } catch {
+                throw MMError.invalidData
+            }
+        }
+    }
+}
