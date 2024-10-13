77b3bb293644cb22b7449e9af95009be739bf2bd
amanjain-ops <jainaman06060@gmail.com>
2024-10-14 02:46:44 +0530

    Model Added for chats view

--- MessageMe/Views/Chats/Model/Chats.swift
+++ MessageMe/Views/Chats/Model/Chats.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+struct Chats: Codable, Identifiable {
+    let msg: String?
+    let id: String
+    let participants: [String]
+    let createdAt: String?
+    let lastMessage: LastMessage?
+}
+
+struct LastMessage: Codable {
+    let message: String
+    let senderId: String
+    let sentAt: Date
+}
