4b491d456dad3372300518297cd02805f1d71e98
amanjain-ops <jainaman06060@gmail.com>
2024-10-14 02:47:20 +0530

    Added view model for ChatView

--- MessageMe/Views/Chats/Model/ChatsViewModel.swift
+++ MessageMe/Views/Chats/Model/ChatsViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class ChatsViewModel: ObservableObject {
+    @Published var chats: [Chats] = []
+//    private var loginViewModel: LoginViewModel?
+    
+    func getChatList(with viewModel: LoginViewModel) {
+        Task { @MainActor in
+            do {
+                chats = try await NetworkManger.shared.getChats(with: viewModel)
+            } catch {
+                throw MMError.invalidResponse
+            }
+        }
+    }
+}
