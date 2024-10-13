00239be9946cd83b3e7904fdac084fe93db22a8d
amanjain-ops <jainaman06060@gmail.com>
2024-10-14 02:50:11 +0530

    New chat create ui added

--- MessageMe/Views/Chats/View/NewChatView.swift
+++ MessageMe/Views/Chats/View/NewChatView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct NewChatView: View {
+    
+    @EnvironmentObject var loginViewModel: LoginViewModel
+    @StateObject var viewModel = NewChatViewModel()
+    @EnvironmentObject var webSocketManager: WebSocketManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if loginViewModel.loginResponsee?.accessToken == nil {
+            LoginView()
+        }
+        
+        else {
+            if webSocketManager.chatCreatedResponse?.id != nil {
+                ChatDetailView(chatId: webSocketManager.chatCreatedResponse?.id ?? "")
+                
+            }
+            else {
+                ScrollView {
+                    if !viewModel.userList.isEmpty{
+                        ForEach(viewModel.userList) { user in
+                            HStack(alignment: .center, spacing: 15) {
+                                
+                                Circle()
+                                    .frame(width: 60)
+                                
+                                // name and last message
+                                VStack (alignment: .leading, spacing: 5){
+                                    Text(user.id)
+                                        .fontWeight(.semibold)
+                                }
+                                Spacer()
+                            }
+                            .padding()
+                            .onAppear{
+                                webSocketManager.configure(with: loginViewModel)
+                            }
+                            .onTapGesture {
+                                print("Hello")
+                                
+                                do {
+                                     
+                                     webSocketManager.chatCreate(recipentId: user.id)
+                                }
+                            }
+                        }
+                    }
+                }
+                .task{
+                    viewModel.getUsers(with: loginViewModel)
+                }
+                .onDisappear{
+                    webSocketManager.disconnect()
+                }
+            }
+        }
     }
 }
 
 #Preview {
     NewChatView()
+        .environmentObject(LoginViewModel())
+        .environmentObject(WebSocketManager())
 }
