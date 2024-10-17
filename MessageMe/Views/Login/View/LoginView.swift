d2d5a01aea6887c19d2e760533dd2ccf272e9b34
amanjain-ops <jainaman06060@gmail.com>
2024-10-17 10:56:49 +0530

    navigation added

--- MessageMe/Views/Login/View/LoginView.swift
+++ MessageMe/Views/Login/View/LoginView.swift
@@ -9,41 +9,47 @@ import SwiftUI
 
 struct LoginView: View {
     @EnvironmentObject var viewModel: LoginViewModel
-    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Login")
-                .font(.title)
-                .fontWeight(.semibold)
-            
-            
-            TextField("Email", text: $viewModel.loginUser.email)
-                .autocapitalization(.none)
-                .frame(width: 350, height: 50)
-                .padding(.horizontal, 10)
-                .background(Color.gray.opacity(0.1))
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-            
-            SecureField("Password", text: $viewModel.loginUser.password)
-                .autocapitalization(.none)
-                .frame(width: 350, height: 50)
-                .padding(.horizontal, 10)
-                .background(Color.gray.opacity(0.1))
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-            
-            
-            Button {
-                viewModel.login()
-            } label: {
+        NavigationStack() {
+            VStack(spacing: 20) {
                 Text("Login")
-                    .foregroundStyle(.white)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                
+                TextField("Email", text: $viewModel.loginUser.email)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
                     .frame(width: 350, height: 50)
-                    .background(.black)
+                    .padding(.horizontal, 10)
+                    .background(Color.gray.opacity(0.1))
                     .clipShape(RoundedRectangle(cornerRadius: 8))
                 
+                SecureField("Password", text: $viewModel.loginUser.password)
+                    .autocapitalization(.none)
+                    .frame(width: 350, height: 50)
+                    .padding(.horizontal, 10)
+                    .background(Color.gray.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                
+                
+                Button {
+                    viewModel.login()
+                } label: {
+                    Text("Login")
+                        .foregroundStyle(.white)
+                        .frame(width: 350, height: 50)
+                        .background(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+                }
+                
+//                Text(viewModel.loginResponsee?.accessToken ?? "No response")
+                HStack {
+                    Text("Don't have an Account?")
+                    NavigationLink("Sign Up", destination: SignupView())
+                }
             }
-            
-            Text(viewModel.loginResponsee?.accessToken ?? "No response")
         }
     }
 }
