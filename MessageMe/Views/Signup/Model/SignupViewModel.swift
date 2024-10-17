0d729dd5440f71cc8cfcf0a01379763ea3051e4a
amanjain-ops <jainaman06060@gmail.com>
2024-10-17 10:49:43 +0530

    changes in call now we check response code

--- MessageMe/Views/Signup/Model/SignupViewModel.swift
+++ MessageMe/Views/Signup/Model/SignupViewModel.swift
@@ -11,12 +11,17 @@ import Foundation
 class SignupViewModel: ObservableObject {
     @Published var user = Signup()
     @Published var response: SignupResponse?
-    
+    @Published var isSignupSuccessful = false
     
     func signup() {
-        Task { @MainActor in 
+        Task { @MainActor in
             do {
-                response = try await NetworkManger.shared.signup(user: user)
+               let  (data, httpResponse) = try await NetworkManger.shared.signup(user: user)
+                response = data
+                if httpResponse.statusCode == 201 {
+                    isSignupSuccessful = true
+                    print(isSignupSuccessful)
+                }
             } catch {
                 print(error.localizedDescription)
                 throw MMError.invalidData
