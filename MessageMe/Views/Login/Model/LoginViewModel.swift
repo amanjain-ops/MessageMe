4c25a48f8d1ed2d982310efba656cc7b91b0c5ea
amanjain-ops <jainaman06060@gmail.com>
2024-10-17 12:42:10 +0530

    Logic for form validation added

--- MessageMe/Views/Login/Model/LoginViewModel.swift
+++ MessageMe/Views/Login/Model/LoginViewModel.swift
@@ -11,16 +11,39 @@ import Foundation
 class LoginViewModel: ObservableObject {
     @Published var loginUser = Login()
     @Published var loginResponsee: LoginResponse?
+    @Published var errorMessage: String = ""
     
     
     func login() {
         
         Task { @MainActor in
+            guard isValidDForm else { return }
             do {
                 loginResponsee = try await NetworkManger.shared.login(user: loginUser)
             } catch {
                 throw MMError.invalidData
             }
         }
+    }
+    
+    var isValidDForm: Bool {
+        guard !loginUser.email.isEmpty, !loginUser.password.isEmpty else {
+//                alertItem = AlertContext.invalidForm
+            DispatchQueue.main.async{
+                self.errorMessage = "Enter all fields"
+                
+            }
+            return false
+        }
+        
+        guard loginUser.email.isValidEmail else {
+//                alertItem = AlertContext.invalidEmail
+            DispatchQueue.main.async{
+                self.errorMessage = "Invalid Email format"
+            }
+            return false
+        }
+        self.errorMessage = ""
+        return true
     }
 }
