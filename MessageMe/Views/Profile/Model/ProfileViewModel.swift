b9b6457ec1450978e595fe2a3535e0f292f1b384
amanjain-ops <jainaman06060@gmail.com>
2024-10-18 14:59:26 +0530

    ViewModel added

--- MessageMe/Views/Profile/Model/ProfileViewModel.swift
+++ MessageMe/Views/Profile/Model/ProfileViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+class ProfileViewModel: ObservableObject {
+    @Published var profile: Profile?
+    @Published var profileName = ""
+    @Published var isShowNameField = false
+    
+    func fetchProfile(with viewLogin: LoginViewModel) {
+        Task { @MainActor in
+            do {
+                profile = try await NetworkManger.shared.getUserInfo(with: viewLogin)
+            }
+            
+        }
+    }
+    
+    func updateName(with viewLogin: LoginViewModel){
+        Task { @MainActor in
+            do {
+                profile = try await NetworkManger.shared.updateName(with: viewLogin, name: profileName)
+                isShowNameField = false
+            }
+            
+        }
+    }
+}
